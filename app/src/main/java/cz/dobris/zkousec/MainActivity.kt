fa8f15a871726af365a5fba63e032c651eb5ff3e
OndrejKulhavy <okulhav@gmail.com>
2020-10-15 18:11:13 +0200

    Main activity - listView integration

--- app/src/main/java/cz/dobris/zkousec/MainActivity.kt
+++ app/src/main/java/cz/dobris/zkousec/MainActivity.kt
@@ -2,6 +2,9 @@ package cz.dobris.zkousec
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,10 +14,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button2.setOnClickListener {
+        val listView = findViewById<ListView>(R.id.listView)
+
+
+
+        val arrayAdapter: ArrayAdapter<String> = ArrayAdapter(
+            this, android.R.layout.simple_list_item_1, Storage.listQFiles(this)
+        )
+        listView.adapter = arrayAdapter
+
+
+        /*
+        //Třeba opravit staré tlačítko za nový list položek
+        val button = findViewById<Button> (R.id.button2)
+        button.setOnClickListener {
             val intent = Intent (this, QuestionPackSetup::class.java)
-            intent.putExtra("FILE_NAME", "example.xml");
             startActivity (intent)
         }
+
+         */
     }
 }
