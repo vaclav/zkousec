7147fabc79b9b71dddc1f94dfa1d8c2d3fc456f1
apesticka <apestticka@gmail.com>
2020-10-15 17:16:23 +0200

    QuestionPackSetup activity added

--- app/src/main/java/cz/dobris/zkousec/MainActivity.kt
+++ app/src/main/java/cz/dobris/zkousec/MainActivity.kt
@@ -1,11 +1,20 @@
 package cz.dobris.zkousec
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button> (R.id.button2)
+        button.setOnClickListener {
+            val intent = Intent (this, QuestionPackSetup::class.java)
+            startActivity (intent)
+        }
     }
 }
---
+++ app/src/main/java/cz/dobris/zkousec/QuestionPackSetup.kt
@@ -0,0 +1,11 @@
+package cz.dobris.zkousec
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionPackSetup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_pack_setup2)
+    }
+}
