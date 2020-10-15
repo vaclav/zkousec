bc9f6ef4fd815ba95d56017fda1a569a0203ea4d
Vaclav <vaclav.pech@gmail.com>
2020-10-15 17:27:50 +0200

    TextAppearance added

--- app/src/main/java/cz/dobris/zkousec/MainActivity.kt
+++ app/src/main/java/cz/dobris/zkousec/MainActivity.kt
@@ -2,8 +2,8 @@ package cz.dobris.zkousec
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button> (R.id.button2)
-        button.setOnClickListener {
+        button2.setOnClickListener {
             val intent = Intent (this, QuestionPackSetup::class.java)
             startActivity (intent)
         }
