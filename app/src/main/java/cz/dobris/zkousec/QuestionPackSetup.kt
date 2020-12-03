8e0b0452310f01f30a309eae65886e15212cad5a
Vaclav <vaclav.pech@gmail.com>
2020-12-03 16:00:23 +0100

    Reorganize the setup activity, add a text view for the remaining question count

--- app/src/main/java/cz/dobris/zkousec/QuestionPackSetup.kt
+++ app/src/main/java/cz/dobris/zkousec/QuestionPackSetup.kt
@@ -4,6 +4,7 @@ import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.os.PersistableBundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -14,70 +15,81 @@ import kotlinx.android.synthetic.main.activity_question_pack_setup2.*
 import kotlin.concurrent.thread
 
 class QuestionPackSetup : AppCompatActivity() {
+    lateinit var fileName : String
+    lateinit var session: TestSession
 
     override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
         super.onSaveInstanceState(outState, outPersistentState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        fileName = intent.getStringExtra("FILE_NAME") ?: ""
+
+        val qp = Storage.loadQFile(fileName, this)
+        Log.d("Zkousec", "Loaded: " + qp.id + ":" + qp.version)
+        TitleText.text = fileName.replace(".xml", "")
+        QuestionCount.text = "Počet otázek v sadě: " + qp.questions.size
+
+        val handler = Handler()
+        thread {
+            val db = DBHelper.instance(this)
+            val loaded = db.sessionDao().loadAllById(fileName)
+            session = if (loaded != null) {
+                Log.d("Zkousec", "Reusing a session")
+                TestSession.fromSessionEntity(qp, loaded)
+            } else {
+                val newSession = TestSession(qp)
+                db.sessionDao().insert(newSession.toSessionEntity())
+                Log.d("Zkousec", "Created new session")
+                newSession
+            }
+            Log.d("Zkousec", "Loaded: " + loaded)
+            Log.d("Zkousec", "Total: " + session.totalQuestions())
+            Log.d("Zkousec", "To process: " + session.remainingQuestions())
+            Log.d("Zkousec", "Correct: " + session.correctlyAnsweredQuestions())
+            Log.d("Zkousec", "Incorrect: " + session.incorrectlyAnsweredQuestions())
+            handler.post {
+                //TODO show the numbers of correctly/incorrectly answered questions as well as the remaining ones
+                ToProcessCount.text = "Zbývající otázky: " + session.remainingQuestions().toString()
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question_pack_setup2)
 
-        val fileName = intent.getStringExtra("FILE_NAME")
+        StartButton.setOnClickListener {
+            val nextQuestion = session.nextQuestion()
+            session.evaluateAnswer(nextQuestion.question.answers[0])
+            //TODO start a testing activity
+        }
+        DeleteButton.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("Delete file")
+                .setMessage("Do you really want to delete the questions titled: " + fileName + "?")
+                .setPositiveButton("Yes",
+                    DialogInterface.OnClickListener { dialog, which ->
+                        Storage.deleteQFile(fileName, it.context)
+                        thread {
+                            val db = DBHelper.instance(this)
+                            val loaded = db.sessionDao().loadAllById(fileName)
+                            db.sessionDao().delete(loaded)
+                        }
 
-
-        if(fileName!=null) {
-            val qp = Storage.loadQFile(fileName, this)
-            Log.d("Zkousec", "Loaded: " + qp.id + ":" + qp.version)
-            TitleText.text = fileName.replace(".xml","")
-            QuestionCount.text = "Počet otázek v sadě: " + qp.questions.size
-            //TODO show the numbers of correctly/incorrectly answered questions as well as the remaining ones
-            StartButton.setOnClickListener {
-
-                //TODO start a testing activity
-                thread {
-                    val db = DBHelper.instance(this)
-                    val loaded = db.sessionDao().loadAllById(fileName)
-                    val session = if (loaded != null) {
-                        Log.d("Zkousec", "Reusing a session")
-                        TestSession.fromSessionEntity(qp, loaded)
-                    } else {
-                        val newSession = TestSession(qp)
-                        db.sessionDao().insert(newSession.toSessionEntity())
-                        Log.d("Zkousec", "Created new session")
-                        newSession
-                    }
-                    Log.d("Zkousec", "Loaded: " + loaded)
-                    Log.d("Zkousec", "Total: " + session.totalQuestions())
-                    Log.d("Zkousec", "To process: " + session.remainingQuestions())
-                    Log.d("Zkousec", "Correct: " + session.correctlyAnsweredQuestions())
-                    Log.d("Zkousec", "Incorrect: " + session.incorrectlyAnsweredQuestions())
-                }
-            }
-            DeleteButton.setOnClickListener {
-                AlertDialog.Builder(this)
-                    .setTitle("Delete file")
-                    .setMessage("Do you really want to delete the questions titled: " + fileName + "?")
-                    .setPositiveButton("Yes",
-                        DialogInterface.OnClickListener { dialog, which ->
-                            Storage.deleteQFile(fileName, it.context)
-                            thread {
-                                val db = DBHelper.instance(this)
-                                val loaded = db.sessionDao().loadAllById(fileName)
-                                if(loaded != null) db.sessionDao().delete(loaded)
-                            }
-
-                            val intent = Intent (this, MainActivity::class.java)
-                            startActivity(intent)
-                        })
-
-                    .setNegativeButton("No", DialogInterface.OnClickListener { dialog, which ->
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
                     })
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .show()
 
-
-            }
+                .setNegativeButton("No", DialogInterface.OnClickListener { dialog, which ->
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show()
         }
     }
 }
