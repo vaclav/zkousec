a177437ef2fdac9497d0e52c6c25e7fc49e1acff
Vaclav <vaclav.pech@gmail.com>
2020-12-17 17:42:26 +0100

    toString on TextualAnswer

--- app/src/main/java/cz/dobris/zkousec/domain/Answer.kt
+++ app/src/main/java/cz/dobris/zkousec/domain/Answer.kt
@@ -5,7 +5,7 @@ open class Answer(val correct: Boolean = false) {
 }
 
 class TextualAnswer (correct: Boolean, val text : String) : Answer (correct) {
-
+    override fun toString(): String = text
 }
 
 class ImageAnswer (val url : String) : Answer () {
