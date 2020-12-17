cc439c99017fafd3aaa1f5254be007b992ec7d1f
OndrejKulhavy <okulhav@gmail.com>
2020-12-17 19:13:53 +0100

    QuestionPackTesting.kt update

--- app/src/main/java/cz/dobris/zkousec/QuestionPackTesting.kt
+++ app/src/main/java/cz/dobris/zkousec/QuestionPackTesting.kt
@@ -3,6 +3,8 @@ package cz.dobris.zkousec
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.widget.Button
 import cz.dobris.zkousec.db.DBHelper
 import cz.dobris.zkousec.domain.TestSession
 import kotlinx.android.synthetic.main.activity_question_pack_testing.*
@@ -28,6 +30,7 @@ class QuestionPackTesting : AppCompatActivity() {
                 QuestionText.text = session.nextQuestion().question.text
                 setTitle(fileName)
                 RemainingQuestionsText.text = "Remaining questions: " + session.remainingQuestions().toString()
+                setAnswerButtonsText()
             }
         }
         NextQuestionButton.setOnClickListener {
@@ -38,11 +41,50 @@ class QuestionPackTesting : AppCompatActivity() {
                 DBHelper.saveTestSession(this, session)
                 // TODO
                 handler.post{
+                    setAnswerButtonsText()
                     QuestionText.text = session.nextQuestion().question.text
                     setTitle(fileName)
                     RemainingQuestionsText.text = "Remaining questions: " + session.remainingQuestions().toString()
                 }
             }
         }
+    }
+            fun setAnswerButtonsText(){
+                val numberOfQuestion = session.nextQuestion().question.answers.size
+                AnswerButton1.visibility = View.VISIBLE
+                AnswerButton2.visibility = View.VISIBLE
+                AnswerButton3.visibility = View.VISIBLE
+                AnswerButton4.visibility = View.VISIBLE
+                when(numberOfQuestion){
+                    1 -> {
+                        /// TODO: error message
+                    }
+                    2 -> {
+                        setAnswerButtonsTextHelper(AnswerButton1, 0)
+                        setAnswerButtonsTextHelper(AnswerButton2, 1)
+                        AnswerButton3.visibility = View.GONE;
+                        AnswerButton4.visibility = View.GONE;
+
+                    }
+                    3 -> {
+                        setAnswerButtonsTextHelper(AnswerButton1, 0)
+                        setAnswerButtonsTextHelper(AnswerButton2, 1)
+                        setAnswerButtonsTextHelper(AnswerButton3, 2)
+                        AnswerButton4.visibility = View.GONE;
+                    }
+                    4 -> {
+                        setAnswerButtonsTextHelper(AnswerButton1, 0)
+                        setAnswerButtonsTextHelper(AnswerButton2, 1)
+                        setAnswerButtonsTextHelper(AnswerButton3, 2)
+                        setAnswerButtonsTextHelper(AnswerButton4, 3)
+                    }
+                    else -> {
+                        error("Buttons text setting failed")
+                    }
+                }
+            }
+    fun setAnswerButtonsTextHelper(answerButton : Button, questionNumber : Int){
+
+        answerButton.text = session.nextQuestion().question.answers[questionNumber].toString()
     }
 }
